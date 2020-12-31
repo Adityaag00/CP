976295ea72e6e5b509b76d838354180303b04eda
Adityaag00 <76404748+Adityaag00@users.noreply.github.com>
2020-12-31 19:21:49 +0530

    Create 2Dto1D.java

---
+++ 2Dto1D.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'Spiral' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts 2D_INTEGER_ARRAY a as parameter.
+     */
+
+    public static List<Integer> Spiral(List<List<Integer>> a) {
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int M = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> a = new ArrayList<>();
+
+        IntStream.range(0, M).forEach(i -> {
+            try {
+                a.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = Result.Spiral(a);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
